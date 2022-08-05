248d5d7a6337367fc66b9696409d4accdbcec98f
donnyrla10 <kokoo1214@naver.com>
2022-08-05 14:20:24 +0900

    1182.cpp

--- BOJ/1182.cpp
+++ BOJ/1182.cpp
@@ -17,7 +17,6 @@ int main(){
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     cin >> n >> s;
     for(int i=0; i<n; i++) cin >> arr[i];
-    sort(arr, arr+n); //정렬
     BT(0, 0);
     if(s==0) ans--;
     cout << ans << '\n';
