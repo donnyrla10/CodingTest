b054ff17fb5aa14d3b3d4a3d8a04e214fec53948
donnyrla10 <kokoo1214@naver.com>
2023-03-09 15:09:00 +0900

    1038.swift

---
+++ BOJ/Swift/1038.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+/*
+ 감소하는 수
+ 정수 X의 자릿수가 가장 큰 자릿수부터 작은 자릿수까지 감소한다면
+ 예를 들어, 321, 950. 322, 958 안됨
+ N번째 감소하는 수를 출력하라.
+ */
+
+//input
+let n = Int(readLine()!)!
+let str = String(n).map{ $0 }
+var visited = [Bool](repeating: false, count: 1000000)
+var cnt = -1
+
+func bt(_ now: Int, _ num: String, _ dest: Int) {
+    if now == dest {
+        cnt += 1
+        if cnt == n {
+            print(num)
+            exit(0)
+        }
+    }
+    
+    for i in 0...9 {
+        if num != "" {
+            if String(num.last!) <= String(i) { continue }
+        }
+        if !visited[i] {
+            visited[i] = true
+            bt(now+1, num + String(i), dest)
+            visited[i] = false
+        }
+    }
+}
+
+for i in 1...10 {
+    bt(0, "", i)
+}
+print(-1)
